1ff96aa04c5881a05f7e56060b94e937194f5b43
dash7ou <fxfddfno@gmail.com>
2023-01-16 22:14:58 +0200

    Delete products

--- main.py
+++ main.py
@@ -58,15 +58,26 @@ def get_product(id: str, response: Response):
 
 
 @app.post("/products")
-def create(product: Product):
-    return product.save()
+def create(product: Product, response: Response):
+    try:
+        return product.save()
+    except:
+        response.status_code = 500
+        return {
+            "error": "Server Error!"
+        }
 
 
-@app.delete("/product/{id}")
-def delete_product(id: int, response: Response):
-    product = get_product_details(id)
-    if (not product):
+@app.delete("/products/{id}")
+def delete_product(id: str, response: Response):
+    try:
+        product = get_product_details(id)
+        return Product.delete(id)
+    except NotFoundError:
         response.status_code = 404
         return {}
-
-    return Product.delete(id)
+    except:
+        response.status_code = 500
+        return {
+            "error": "Server Error!"
+        }
